22fb13b9e99985ca19b8a33ff8b6c7c190e0cef0
sherri010 <aminshahidi.sh@gmail.com>
2017-11-21 22:44:16 -0800

    fix pg verion

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var http = require('http');
 var app = express();
 var server = http.createServer(app);
 var socket = require('socket.io')(server);
-
-var db = require('./database.js');
+// var db = require('./database.js');
+var models = require('./server/models/index');
 
 // end points
 app.use(express.static(__dirname + '/assets'));
@@ -15,6 +15,17 @@ app.get('/', function(req, res) {
 
 socket.on('connect', function(client){
 	console.log('Client connected...');
+	models.Blob.create({
+		name: 'sherri',
+		age: false
+	}).then(function(blob){
+		console.log('created', blob.name, blob.age);
+	});
+
+	models.Blob.findAll({}). then(function(blobs){
+		console.log('blobs', blobs);
+	});
+
 
 	client.on('join', function(data){
 		console.log(data);
