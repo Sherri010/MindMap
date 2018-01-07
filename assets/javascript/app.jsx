72a6ab5ad07bd9340135d76a96bd27dc36557897
Sherri Aminshahidy <aminshahidi.sh@gmail.com>
2018-01-06 22:40:47 -0800

    add react and setup webpack to user babel

---
+++ assets/javascript/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+export default class Hello extends Component {
+  render() {
+    return (
+      <div>
+        Hello from react
+      </div>
+    );
+  }
+}
+render(<Hello />, document.getElementById('app'));
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './javascript/index.js',
@@ -8,18 +9,29 @@ module.exports = {
 		filename: 'bundle.js',
 		publicPath: 'http://localhost:3000',
 	},
-	// module: {
-	// 	rules: [
-	// 		{
-	// 			use: 'babel-loader',
-	// 			test: /\.js$/,
-	// 		},
-	// 	]
-	// },
-	devServer: {
-		// historyApiFallback: true,
-	  // port: 3000,
-		// publicPath: '/dist/',
+	module: {
+		 rules: [
+			 {
+				 // test: /\.(js|jsx)$/,
+				 // exclude: /node_modules/,
+				 // use: [
+					//  'babel-loader',
+				 // ],
+				 test: /.jsx?$/,
+				 loader: 'babel-loader',
+				 exclude: /node_modules/,
+				 query: {
+					 presets: ['es2015', 'react']
+				 }
+			 },
+		 ],
+ },
+	plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.html'),
+    }),
+  ],
+	resolve: {
+		extensions: ['.js', '.jsx'],
 	},
-	// watch: true,
 }
