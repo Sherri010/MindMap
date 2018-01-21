f2899d54084dc94b98cb5be19f2d6acb3e63891f
Sherri Aminshahidy <aminshahidi.sh@gmail.com>
2018-01-20 22:05:40 -0800

    redirect to personalDashboard after auth

--- assets/javascript/app.jsx
+++ assets/javascript/app.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
 import ReactDOM  from 'react-dom';
 import HomePage from './mainPages/HomePage/index.jsx';
+import PersonalDashboard from './mainPages/PersonalDashboard/index.jsx';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 ReactDOM.render((
   <Router>
     <div>
       <Route exact path="/" component={HomePage}/>
+      <Route path="/personalLibrary" component={PersonalDashboard}/>
     </div>
   </Router>
 ), document.getElementById('app'));
-
-
-{/* <div>
-  <h3>MindMap</h3>
-  <button id='createNoteBook'>
-    create New NoteBook
-  </button>
-  <div id='list'></div>
-  <div>
-    <a href='/login'> Login </a>
-  </div>
-  <div>
-    <a href='/logout'> Logout </a>
-  </div>
-</div> */}
