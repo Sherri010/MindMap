4299694a3b5753dcdd946814821e9c4eb3f7aa1e
Sherri Aminshahidy <aminshahidi.sh@gmail.com>
2018-03-01 23:23:17 -0800

    fix socket connection and session access

--- assets/javascript/mainPages/PersonalDashboard/index.jsx
+++ assets/javascript/mainPages/PersonalDashboard/index.jsx
@@ -1,10 +1,43 @@
 import React,{ Component } from 'react';
 import PropTypes from 'prop-types';
+import { getUser } from '../../sockets/users';
+
+let user;
+
 
 export default class PersonalDashboard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user: {},
+        }
+    }
+    componentDidMount(){
+        // $.ajax({
+        //    type: 'GET',
+        //    url: 'http://localhost:4567/user',
+        //    error: function() {
+        //       console.log('error on ajax')
+        //    },
+        //    success: function(data) {
+        //        console.log(data);
+        //        user = data;
+        //    }
+        // });
+
+        getUser(function getUserCallback(res){
+            user = console.log(res)
+        });
+    }
+
     render(){
+        const { user } = this.state;
+        console.log('redern', user)
+        // const { lastName } = user;
         return (
-            <p>PErSONAL LIBRARY</p>
+            <div>
+                <h3>Personal Dashboard</h3>
+            </div>
         );
     }
 }
