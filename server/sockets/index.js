029b74287e139844dc5fb527ba2a4ff4ed2fc6cb
Sherri Aminshahidy <aminshahidi.sh@gmail.com>
2018-04-22 18:13:48 -0700

    cleanup

--- server/sockets/index.js
+++ server/sockets/index.js
@@ -25,7 +25,6 @@ io
                  cb(user)
             }
             else {
-                // res.status(500).send('No User Found');
                 throw new Error('User not found');
             }
         });
@@ -52,85 +51,3 @@ io
     });
 
 });
-// users.on('connection', function(){
-//     console.log('USER connected...');
-//
-//     users.on('get', function(data, cb){
-// 		console.log('USER SOCKET', socket.request.session)
-// 		let user;
-// 		models.User.find({
-// 			where: {
-// 				id: socket.request.session.userId,
-// 			}
-// 		}).then(function(user){
-// 			if(user){
-// 				 cb(user)
-// 			}
-// 			else {
-// 				// res.status(500).send('No User Found');
-// 				cb('No User Found')
-// 			}
-// 		});
-// 	});
-// });
-
-// io.on('connection', function(socket){
-// 	console.log('Client connected...');
-//
-//     users.on('get', function(data, cb){
-//         console.log(' --------- GET', data)
-//     });
-//
-//     notebooks.on('post', function(data, cb){
-//         console.log(' --------- POST', data)
-//
-//     });
-//
-//     notebooks.on('search', function(data, cb){
-//         console.log(' --------- SEARCH', data)
-//
-//     });
-
-	// socket.on('user', function(data, cb){
-	// 	console.log('USER SOCKET', socket.request.session)
-	// 	let user;
-	// 	models.User.find({
-	// 		where: {
-	// 			id: socket.request.session.userId,
-	// 		}
-	// 	}).then(function(user){
-	// 		if(user){
-	// 			 cb(user)
-	// 		}
-	// 		else {
-	// 			// res.status(500).send('No User Found');
-	// 			cb('No User Found')
-	// 		}
-	// 	});
-	// });
-    //
-    // socket.on('userNoteBooks', function({ UserId }, cb){
-    //     models.Notebook.findAll({
-    //         where: {
-    //             name: 'sherri sockets',
-    //         }
-    //     }).then(function(notebooks = []){
-    //             if(notebooks){
-    //                 cb({ notebooks })
-    //             }
-    //             else {
-    //                 cd([]);
-    //             }
-    //         });
-    // });
-    //
-    // socket.on('postNotebook', function({ name }, cb){
-    //     models.Notebook
-    //     .build({ name })
-    //     .save()
-    //     .then(function(notebook){
-    //             cb({ notebook });
-    //     })
-    //     .catch(error => console.log(error))
-    // });
-// });
